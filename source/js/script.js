2dde2941df126448bd3e0ba551a286767b159258
Benjamin König <bkoenig.as@gmail.com>
2022-05-24 15:17:10 +0200

    add: plane

--- source/js/script.js
+++ source/js/script.js
@@ -3,15 +3,10 @@ import * as THREE from '../../node_modules/three/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 
-/*const reference = document.querySelector("#my_canvas");
-
-const renderer = new THREE.WebGLRenderer({
-    antialias:true,
-    canvas: reference
-});*/
-
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+
 document.body.appendChild( renderer.domElement );
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -19,18 +14,21 @@ const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
 
 const mesh = new THREE.Mesh(boxGeometry, material);
 
-
 scene.add(mesh)
 
 camera.position.z = 5
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide})
+const planeMesh =   new THREE.Mesh(
+    planeGeometry, planeMaterial)
+ scene.add(planeMesh)
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
     mesh.rotation.x += 0.01
     mesh.rotation.y += 0.01
-
+    planeMesh.rotation.x += 0.01
 }
 animate()
-
-
