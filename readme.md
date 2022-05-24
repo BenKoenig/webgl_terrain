195287db93def3672bd7a51bc299711ed6c92f28
Benjamin König <bkoenig.as@gmail.com>
2022-05-24 16:00:14 +0200

    add: gui, update: readme

--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # Three.js Experimental
 
 ## Technologies
-- Three.js
-- TailwindCSS
+- [Three.js](https://threejs.org/)
+- [TailwindCSS](https://tailwindcss.com/)
+
+## Tutorial
+I'm still very new to WebGL and I've created this project by watching [Chris Courses](https://www.youtube.com/watch?v=YK1Sw_hnm58&t=253s) tutorial on [Three.js](https://threejs.org/).
+
+## Demo
+Soon
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,27 @@
-import * as THREE from '../../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI()
+const world = {
+    plane: {
+        width: 10,
+        height: 10
+    }
+}
+gui.add(world.plane, 'width', 1, 500)
+    .onChange(() => {
+        planeMesh.geometry.dispose()
+        planeMesh.geometry =  new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10);
+
+        const { array } = planeMesh.geometry.attributes.position;
+        for (let i = 0; i < array.length; i += 3) {
+            const x  = array[i];
+            const y  = array[i + 1 ];
+            const z  = array[i + 2 ];
+
+            array[i + 2] = z + Math.random()
+        }
+    })
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
@@ -9,26 +32,41 @@ renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild( renderer.domElement );
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-
-const mesh = new THREE.Mesh(boxGeometry, material);
-
-scene.add(mesh)
-
 camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide})
+const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading
+})
 const planeMesh =   new THREE.Mesh(
     planeGeometry, planeMaterial)
  scene.add(planeMesh)
 
+const { array } = planeMesh.geometry.attributes.position;
+
+
+for (let i = 0; i < array.length; i += 3) {
+    const x  = array[i];
+    const y  = array[i + 1 ];
+    const z  = array[i + 2 ];
+
+    array[i + 2] = z + Math.random()
+}
+
+
+const light = new THREE.DirectionalLight(
+    0xffffff, 1
+)
+
+light.position.set(0, 0, 1)
+scene.add(light)
+
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    mesh.rotation.x += 0.01
-    mesh.rotation.y += 0.01
-    planeMesh.rotation.x += 0.01
+
+/*    planeMesh.rotation.x += 0.01*/
 }
 animate()
