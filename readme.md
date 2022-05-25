de05e54df5abf41dbb6b36d6bb0ad48dd62bb37c
Benjamin König <bkoenig.as@gmail.com>
2022-05-25 14:16:34 +0200

    add: hover animation, update: readme.md

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
-# WebGL Experimental
+# WebGL Terrain
 
 ## Technologies
 - [Three.js](https://threejs.org/) :boom:
 - [TailwindCSS](https://tailwindcss.com/) :blue_heart:
 - [CodeKit](https://codekitapp.com/) :fish:
+- [GSAP](https://greensock.com/gsap/) :dog:
 
 ## Tutorial
 Learning [Three.js](https://threejs.org/) by watching [Chris Courses](https://www.youtube.com/watch?v=YK1Sw_hnm58&t=253s) tutorial on YouTube.
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as gsap from 'gsap';
 import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' 
 
@@ -43,7 +44,7 @@ gui.add(world.plane, 'widthSegments', 1, 20)
 gui.add(world.plane, 'heightSegments', 1, 20)
     .onChange(generatePlane)
 
-const raycaster = new THREE.Raycaster()
+const raycaster = new THREE.Raycaster
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, 
@@ -65,11 +66,11 @@ camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0xff0000,
     side: THREE.DoubleSide,
     flatShading: THREE.FlatShading,
-    vertexColors: true;
+    vertexColors: true
 })
+
 const planeMesh = new THREE.Mesh(
     planeGeometry, planeMaterial)
 scene.add(planeMesh)
@@ -85,8 +86,19 @@ for (let i = 0; i < array.length; i += 3) {
     array[i + 2] = z + Math.random()
 }
 
+
+
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4)
+}
+
+
+
 planeMesh.geometry.setAttribute('color',
-    new THREE.BufferAttribute(new Float32Array([0, 0, 1]), 3))
+    new THREE.BufferAttribute(new Float32Array(colors), 3))
+
+
 
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
@@ -109,11 +121,6 @@ function animate() {
 
     renderer.render(scene, camera)
 
-    raycaster.setFromCamera(mouse, camera)
-    const intersects = raycaster.intersectObject(planeMesh)
-
-
-
 
     /*    planeMesh.rotation.x += 0.01*/
 }
@@ -121,8 +128,50 @@ function animate() {
 animate()
 
 
-
 addEventListener('mousemove', (e) => {
     mouse.x = (e.clientX / innerWidth) * 2 - 1
     mouse.y = -(e.clientY / innerHeight) * 2 + 1
+
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObject(planeMesh)
+
+    if(intersects.length > 0) {
+
+
+        const { color } = intersects[0].object.geometry.attributes
+
+        // verticle 1 
+        color.setX(intersects[0].face.a, 0.1)
+        color.setY(intersects[0].face.a, 0.5)
+        color.setZ(intersects[0].face.a, 1)
+
+        // verticle 2
+        color.setX(intersects[0].face.b, 0.1)
+        color.setY(intersects[0].face.b, 0.5)
+        color.setZ(intersects[0].face.b, 1)
+
+        // verticle 3
+        color.setX(intersects[0].face.c, 0.1)
+        color.setY(intersects[0].face.c, 0.5)
+        color.setZ(intersects[0].face.c, 1)
+
+        intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+
+        const initialColor = {
+            r: 0,
+            g: .19,
+            b: .4
+        }
+
+        const hoverColor = {
+            r: 0.1,
+            g: .5,
+            b: 1
+        }
+
+    }
+
+
+
 })
